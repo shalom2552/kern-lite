967849697a2eda073ee054d25ad7dc8f1720f0ae
Yair-Dekel <ydekel10@campus.haifa.ac.il>
2026-07-02 16:12:56 +0300

    style(frame): modify values

--- firmware/protocol/frame.hpp
+++ firmware/protocol/frame.hpp
@@ -2,13 +2,12 @@
 
 #include <cstdint>
 #include <cstddef>
-
 namespace kern::protocol {
 
 inline constexpr uint8_t kStx = 0xAB;
 inline constexpr uint8_t kEtx = 0xCD;
 inline constexpr size_t kMaxPayload = 256;
-inline constexpr size_t kFrameOverhead = 8;
+inline constexpr size_t kFrameOverhead = 9;
 
 enum class FrameType : uint8_t {
     CmdStart = 0x01,
@@ -16,18 +15,18 @@ enum class FrameType : uint8_t {
     CmdStatus = 0x03,
     CmdReplay = 0x04,
     CmdErase = 0x06,
-    Status = 0x10,
-    Record = 0x11,
+    Record = 0x10,
+	Status = 0x12,
     Ack = 0x20,
     Nack = 0x21,
 };
 
 enum class NackCode : uint8_t {
-    BadCommand = 1,
-    InvalidState = 2,
-    BadLength = 3,
-    BadMagic = 4,
-    StorageError = 5,
+	CrcError = 1,
+    BadCommand = 2,
+    InvalidState = 3,
+    StorageError = 4,
+    BadMagic = 6,
 };
 
 struct Frame {
